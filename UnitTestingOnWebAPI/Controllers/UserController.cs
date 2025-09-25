1f538fbe460b6b8276ebbdf1c794fa601ab1bf60
Nathan Marques <nathanmarques.silva@gmail.com>
2025-09-25 19:23:37 -0300

    Feat: Alguns endpoints adicionados

---
+++ UnitTestingOnWebAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Models;
+using API.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController(IUserInterface userInterface) : ControllerBase
+    {
+        // Create
+        [HttpPost("add")]
+
+        public async Task<IActionResult> Create(User user)
+        {
+            var result = await userInterface.CreateAsync(user);
+            if (result)
+                return CreatedAtAction(nameof(Create), new { id = user.Id }, user);
+            else
+                return BadRequest();
+        }
+
+        // Read All
+        [HttpPost("get")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await userInterface.GetAllAsync();
+            if (!users.Any())
+                return NotFound();
+            else
+                return Ok(users);
+        }
+    }
+}

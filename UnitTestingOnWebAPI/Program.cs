4fc69ba7cc0eb445a6edfbc21264ae5886afb6b4
Nathan Marques <nathanmarques.silva@gmail.com>
2025-09-25 19:23:25 -0300

    Chore: Injeção de dependência adicionada ao IService

--- UnitTestingOnWebAPI/Program.cs
+++ UnitTestingOnWebAPI/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<UserDbContext> (options => options.UseSqlite("Data Source=UnitTestDb.db"));
+builder.Services.AddScoped<IUserInterface, UserRepository>();
 
 var app = builder.Build();
 

492cb09edde12c7316e1dac20fb91872833d35d0
Nathan Marques <nathanmarques.silva@gmail.com>
2025-10-03 11:44:21 -0300

    Feat: Criação de testes unitários em xUnit com Moq e NSubistitute

---
+++ Test/ControllerTests/UserControllerTest.cs
@@ -0,0 +1,190 @@
+﻿using API.Controllers;
+using API.Models;
+using API.Repository;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NSubstitute;
+
+namespace Test.ControllerTests
+{
+    public class UserControllerTest
+    {
+        // Dependencias para os testes ultilizando Moq
+        private readonly Mock<IUserInterface> MockUserInterface;
+        private readonly UserController _userController;
+
+        // Dependencias para os testes ultilizando NSubstitute
+        private readonly IUserInterface SubstituteUserInterface;
+        private readonly UserController _UserController;
+
+        public UserControllerTest()
+        {
+            MockUserInterface = new Mock<IUserInterface>();
+            _userController = new UserController(MockUserInterface.Object);
+
+            SubstituteUserInterface = Substitute.For<IUserInterface>();
+            _UserController = new UserController(SubstituteUserInterface);
+
+        }
+
+        private static User CreateMockUser()
+        {
+            return new User
+            {
+                Id = 1,
+                Name = "Test User",
+                Email = "testuser@email.com"
+            };
+        }
+
+
+        // Testes ultilizando Moq e FluentAssertions
+        [Fact]
+        public async void UserController_Create_ReturnsCreateded()
+        {
+            // Arrange
+            var user = CreateMockUser();
+
+            // Act
+            MockUserInterface.Setup(u => u.CreateAsync(user)).ReturnsAsync(true);
+            var result = (CreatedAtActionResult)await _userController.Create(user);
+
+            // Assert
+            result.StatusCode.Should().Be(201);
+            result.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async void UserController_GetUserById_ReturnsOk(int userId)
+        {
+            // Arrange
+            var user = CreateMockUser();
+
+            // Act
+            MockUserInterface.Setup(u => u.GetByIdAsync(userId)).ReturnsAsync(user);
+            var result = (OkObjectResult)await _userController.GetUserById(userId);
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+            result.Value.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async void UserController_GetUsers_ReturnsOk()
+        {
+            // Arrange
+            var users = new List<User> { CreateMockUser() };
+            MockUserInterface.Setup(u => u.GetAllAsync()).ReturnsAsync(users);
+
+            // Act
+            var result = (OkObjectResult)await _userController.GetUsers();
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+            result.Value.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async void UserController_UpdateUser_ReturnsOk()
+        {
+            // Arrange
+            var user = CreateMockUser();
+            MockUserInterface.Setup(u => u.UpdateAsync(user)).ReturnsAsync(true);
+
+            // Act
+            var result = (OkResult)await _userController.UpdateUser(user);
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async void UserController_DeleteUser_ReturnsOk()
+        {
+            // Arrange
+            int userId = 1;
+            MockUserInterface.Setup(u => u.DeleteAsync(userId)).ReturnsAsync(true);
+
+            // Act
+            var result = (NoContentResult)await _userController.DeleteUser(userId);
+
+            // Assert
+            result.StatusCode.Should().Be(204);
+            result.Should().NotBeNull();
+        }
+
+        // Testes ultilizando NSubstitute sem FluentAssertions
+        [Fact]
+        public async Task UserController_Create_ReturnsBadRequest()
+        {
+            // Arrange
+            var user = CreateMockUser();
+            SubstituteUserInterface.CreateAsync(user).Returns(Task.FromResult(false)); // CreateAsync retorna um objeto Task<bool>, por isso usamos Task.FromResult(false) ao invés de só false
+
+            // Act
+            var result = await _UserController.Create(user);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task UserController_GetUserById_ReturnsNotFound(int userId)
+        {
+            // Arrange
+            SubstituteUserInterface.GetByIdAsync(userId).Returns(Task.FromResult<User?>(null)); // GetByIdAsync retorna um objeto Task<User>, por isso usamos Task.FromResult<User?>(null) ao invés de só null
+
+            // Act
+            var result = await _UserController.GetUserById(userId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UserController_GetUsers_ReturnsNotFound()
+        {
+            // Arrange
+            SubstituteUserInterface.GetAllAsync().Returns(Task.FromResult(Enumerable.Empty<User>())); // GetAllAsync retorna um objeto Task<IEnumerable<User>>, por isso retornamos uma lista vazia com Task.FromResult(Enumerable.Empty<User>())
+
+            // Act
+            var result = await _UserController.GetUsers();
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UserController_UpdateUser_ReturnsNotFound()
+        {
+            // Arrange
+            var user = CreateMockUser();
+            SubstituteUserInterface.UpdateAsync(user).Returns(Task.FromResult(false));
+
+            // Act
+            var result = await _UserController.UpdateUser(user);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UserController_DeleteUser_ReturnsNotFound()
+        {
+            // Arrange
+            int userId = 1;
+            SubstituteUserInterface.DeleteAsync(userId).Returns(Task.FromResult(false));
+
+            // Act
+            var result = await _UserController.DeleteUser(userId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+    }
+}
--- UnitTestingOnWebAPI/Controllers/UserController.cs
+++ UnitTestingOnWebAPI/Controllers/UserController.cs
@@ -1,34 +1,67 @@
-﻿using API.Models;
-using API.Repository;
+﻿using API.Repository;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+        [ApiController]
+        [Route("[controller]")]
     public class UserController(IUserInterface userInterface) : ControllerBase
     {
-        // Create
+        // Criar usuários
         [HttpPost("add")]
-
         public async Task<IActionResult> Create(User user)
         {
             var result = await userInterface.CreateAsync(user);
             if (result)
-                return CreatedAtAction(nameof(Create), new { id = user.Id }, user);
-            else
-                return BadRequest();
+            {
+                return CreatedAtAction(nameof(Create), new { id = user.Id}, user);
+            }
+            return BadRequest("Falha ao criar usuário.");
         }
 
-        // Read All
-        [HttpPost("get")]
+        // Recuperar usuário por ID
+        [HttpGet("get/{id:int}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await userInterface.GetByIdAsync(id);
+            if (user == null)
+                return NotFound("Usuário não encontrado.");
+            else
+                return Ok(user);
+        }
+
+        // Recuperar todos os usuários
+        [HttpGet("get/all")]
         public async Task<IActionResult> GetUsers()
         {
             var users = await userInterface.GetAllAsync();
             if (!users.Any())
-                return NotFound();
+                return NotFound("Nenhum usuário encontrado.");
             else
                 return Ok(users);
+        }
+
+        // Atualizar usuário
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateUser(User user)
+        {
+            var result = await userInterface.UpdateAsync(user);
+            if (result)
+                return Ok();
+            else
+                return NotFound("Usuário não encontrado.");
+        }
+
+        // Deletar usuário
+        [HttpDelete("delete/{id:int}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var result = await userInterface.DeleteAsync(id);
+            if (result)
+                return NoContent();
+            else
+                return NotFound("Usuário não encontrado.");
         }
     }
 }
